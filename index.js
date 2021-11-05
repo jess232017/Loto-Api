e6a968efb6e2c4ccaf2ed60c1ad9ed7e584943de
Jensen Hernández <Jess232016@outlook.com>
2021-11-05 09:02:11 -0600

    fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 
 app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
